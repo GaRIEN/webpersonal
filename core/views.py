0e5ad7922999a749f2f6a7e3c98c8da02d252bb5
ahuamanpowercode <ahuaman@powercode.pe>
2024-03-26 18:30:10 -0500

    agregue tabla de iconos

--- core/views.py
+++ core/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render,HttpResponse,redirect
+from .models import redesSociales, IconRedesSociales
+
 
 # Create your views here.
 
 def home(request):
-    return render(request,'core/home.html')
+    redes = redesSociales.objects.all()
+    curIcon = {red.redSocial: red.redSocial.icon for red in redes}  # Crear un diccionario de red social: ícono
+    return render(request, 'core/home.html', {'curRedes': redes, 'curIcon': curIcon})
+
 def about(request):
     return render(request,'core/about.html')
 def contacto(request):
