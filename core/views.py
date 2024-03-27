3868a1b2fd99e22d067ea7a43481d03881e5d427
irenedi <irenediaz1305@gmail.com>
2024-03-27 00:12:12 -0500

    cree una app redessociales

--- core/views.py
+++ core/views.py
@@ -1,21 +1,15 @@
 from django.shortcuts import render
-from .models import redesSociales, IconRedesSociales
+# from .models import redesSociales, IconRedesSociales
+
+# def home(request):
+#     # redes = redesSociales.objects.all()
+            
+#     return render(request, 'core/home.html', {'curRedes': redes})
+
 
 def home(request):
-    redes = redesSociales.objects.all()
-    
-    # Iterar sobre cada red social y buscar su imagen correspondiente en IconRedesSociales
-    for red in redes:
-        try:
-            # Buscar la imagen correspondiente en IconRedesSociales basada en el nombre de la red social
-            icono = IconRedesSociales.objects.get(nombre=red.redSocial)
-            # Asignar la imagen encontrada a la red social actual
-            red.imagen = icono.icon
-        except IconRedesSociales.DoesNotExist:
-            # Si no se encuentra una imagen, asignar None
-            red.imagen = None
-    
-    return render(request, 'core/home.html', {'curRedes': redes})
+       
+   return render(request, 'core/home.html')
 
 def about(request):
     return render(request, 'core/about.html')
