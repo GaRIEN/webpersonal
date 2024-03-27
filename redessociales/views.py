317f845c5e7cc89c7acf9d0899bad6cff755d2ee
“GaRIEN” <gabrielhuaman683@gmail.com>
2024-03-27 01:21:33 -0500

    intentando desde redessociales

--- redessociales/views.py
+++ redessociales/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
 
-# Create your views here.
+from django.shortcuts import render
+from .models import IconRedesSociales, redesSociales
+
+
+def redes(request):
+    redes = redesSociales.objects.all()
+    # Pasar los datos al contexto de renderizado
+    return render(request, 'core/base.html', {'curRedes': redes})
